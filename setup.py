e78d2798d08e57ead5fd9bdf460ccb37a668b1c1
Alex.F <gfreezy@gmail.com>
2015-12-22 11:01:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
     install_requires=[
         # quickfix to prevent pep8 conflicts
         'flake8',
+        'flake8-respect-noqa',
         'argparse',
         'autopep8>=1.1',
         'pyyaml',
